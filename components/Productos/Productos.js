7c22482104b89842d89e218f18fccf304676aae9
rominaADiaz <62668803+rominaADiaz@users.noreply.github.com>
2023-11-21 10:44:49 -0300

    Revert "Se eliminan comentarios"

--- components/Productos/Productos.js
+++ components/Productos/Productos.js
@@ -65,12 +65,12 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   productName: {
-    fontSize: 18, 
-    fontWeight: 'bold', 
-    marginBottom: 5, 
+    fontSize: 18, // Tamaño más grande para el nombre del producto
+    fontWeight: 'bold', // Texto en negrita para resaltar
+    marginBottom: 5, // Espacio entre el nombre y el precio
   },
   price: {
-    fontSize: 14,
+    fontSize: 14, // Tamaño más pequeño para el precio
     marginBottom: 5,
   },
 });
